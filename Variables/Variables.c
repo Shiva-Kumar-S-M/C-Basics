18180f6a1642f65af376f4983358779452928911
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2024-09-26 18:43:59 +0530

    Datatypes

--- Variables/Variables.c
+++ Variables/Variables.c
@@ -10,17 +10,66 @@
 // 3.no comma/blank space
 // 4.no other symbol other than '_'
 
-# include <stdio.h>
-int main()
-{
-    int number = 25;
-    char star= '*';
-    int age =22;
-    age = 25;
-    printf("%d\n",age);
-    int final_price=2550;
-    printf("%d\n",final_price);
-}
+// # include <stdio.h>
+// int main()
+// {
+//     int number = 25;
+//     char star= '*';
+//     int age =22;
+//     age = 25;
+//     printf("%d\n",age);
+//     int final_price=2550;
+//     printf("%d\n",final_price);
+// }
 
 
-// Data types
+// Data types in -c 
+// 1.int int age = 24;
+// 2.char char name = 'A';
+// 3.float float price = 25.50;
+
+//Constants - 
+// 1. A constant is a value that cannot be changed.
+// 2. A constant is declared using the const keyword.
+// 3. A constant can be a variable, a function, or a type.
+//3 types
+//1.integer  constant ex:2,3,0,-12
+//2.real constant ex:2.5,3.0,-12.5
+//3. Character  constant ex:'A','a','0'
+
+//keywords - Reserved  words in c that has special  meaning for the  compiler
+// 32 keywords in c
+//Ex- int,float,char,if,do,while,return
+
+//  Understanding program structure
+//  #include<stdio.h>   //Header file--Preprosser directive
+//  int main(){                                  //case sensitive
+//     printf("Hello  World"); // line of code that  performs some action
+
+//     return 0;         // 0-zero errors
+
+//  }
+
+// #include<stdio.h>
+// int main(){
+//     int age= 25;
+//     printf("My age is %d\n",age);     // %d is a placeholder for integer-- formate specifier                         // printf is library  function---. ;-this line ends here/fullstop
+//     float  price = 25.50;
+//     printf("My price is %f\n",price);  // %f is a placeholder for  f--float
+//     char  name = 'A';
+//     printf("My name is %c\n",name);    // %c is a placeholder for  c-character
+//      return 0;
+// }
+
+// commments is line that is not part of code but explains what that line is actually going to do.
+// Single line  
+/* for mutliple line*/
+
+
+
+
+
+
+
+
+
