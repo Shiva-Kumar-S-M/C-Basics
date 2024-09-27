955e17d23b960ed0ed129ebfc6889cb9d5126403
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2024-09-27 19:14:51 +0530

    Instruction

---
+++ Instruction & Operators/Instructions.c
@@ -0,0 +1,47 @@
+// #include<stdio.h>
+// int main (){
+//     // int a =(int) 1.999999;
+//     // printf("%d", a);
+//     // int a = 4+9*10;
+//     // printf("%d", a);
+//     // int a = 4*3/6*2;
+//     // printf("%d", a);
+//     // int a = 5+2/2*3;
+//     printf("%d", 4!=4);
+//     return 0;
+// }
+
+// WAP to check whether number is divisble by 2
+// #include<stdio.h>
+// int main(){
+//     int x; int y=x;
+// //     printf("enter a number:");  // 1-true //0-false
+// //     scanf("%d",&x);
+// //     printf("%d", x%2 ==0);
+//  }
+
+// isSunday & isSnowing 
+// #include<stdio.h>
+// int main() {
+//     int isSunday = 1;
+//     int isSnowing =1;
+//     printf("%d", isSunday && isSnowing);
+// }
+
+// IsMonday or IsRaining
+// #include<stdio.h>
+// int main(){
+//     int isMonday =1;
+//     int isRaining=1;
+//     printf("%d", isMonday || isRaining);
+// }
+
+#include<stdio.h>
+int main(){
+    int x ;
+    printf("Enter a number:");
+    scanf("%d",&x);
+    printf("%d", x>9 && x<100);
+
+
+}
