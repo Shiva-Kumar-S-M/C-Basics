fc880a1551200e2e3df35317011156e1fece2b0e
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2024-10-02 16:11:53 +0530

    Sum

--- Functions & Recuursion/Function.c
+++ Functions & Recuursion/Function.c
@@ -11,3 +11,65 @@
 // void printHello(){
 //     printf("Hello World");
 // }
+
+// #include<stdio.h>
+//  void printHello();
+//  void printBye();
+
+//  int main(){
+//     printHello();
+//     printBye();
+//     return 0;
+//  }
+
+//  void printHello(){
+//     printf("Hello! \n");
+//  }
+//  void printBye(){
+//     printf("Bye!");
+//  }
+
+// WAP to print namaste if the user is indian And bonjour if  the user is french
+// #include<stdio.h>
+// void namste();
+// void  bonjour();
+
+// int main(){
+//     printf("Enter i for indian & f for french:");
+//     char ch;
+//     scanf("%c",&ch);
+
+//     if(ch =='i'){
+//         namste();
+//     }
+//     else{
+//         bonjour();
+//     }
+// }
+
+// void namste(){
+//     printf("Namaste");
+// }
+// void bonjour(){
+//     printf("Bonjour");
+// }
+
+// WAP to  print the sum of two numbers using function
+// #include<stdio.h>
+
+// int sum(int a,int b);
+
+// int main(){
+//     int a , b;
+//     printf("Enter First Number:");
+//     scanf("%d",&a);
+//     printf("Enter Second Number:");
+//     scanf("%d",&b);
+//     int res = sum(a,b);
+//     printf("Sum of Two Numbers is %d:",res);
+
+// }
+
+// int sum(int a,int b){
+//    return a+b;
+// }
