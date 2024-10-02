82fa410648b819d773f5bfa02f18dfd87f6bab88
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2024-10-02 15:40:41 +0530

    Functions

---
+++ Functions & Recuursion/Function.c
@@ -0,0 +1,13 @@
+// Function Syntax
+// #include<stdio.h>
+// void printHello(); //function declaration --prototype
+
+// int main(){
+//     printHello();         //function call 
+//     return 0;
+// }
+
+// //function Defination
+// void printHello(){
+//     printf("Hello World");
+// }
