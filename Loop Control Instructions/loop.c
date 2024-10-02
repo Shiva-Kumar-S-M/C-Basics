665749d3073a57a4a8ee3cf3495881ede5484186
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2024-10-02 10:53:45 +0530

    while

--- Loop Control Instructions/loop.c
+++ Loop Control Instructions/loop.c
@@ -41,3 +41,30 @@
 //         printf("%c \n" , ch);
 //     }
 // }
+
+//----------------------- While loop --------------
+// #include<stdio.h>
+// int main(){
+//     int i = 1;
+//     while(i<=10){
+//         printf("%d \n",i);
+//         i++;
+//     }
+// }
+ 
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number:");
+    scanf("%d \n",&n);
+    // int i=1;
+
+    // while(i<=n){
+    //     printf("%d \n",i);
+    //     i++;
+    // }
+    for ( int i = 0; i<=n ; i++){
+        printf("%d \n", i);
+    }
+}
