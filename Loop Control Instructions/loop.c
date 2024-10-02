16947c67e32fff688737e452cae05128137d1ac6
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2024-10-02 10:21:08 +0530

    for

---
+++ Loop Control Instructions/loop.c
@@ -0,0 +1,43 @@
+// --------------For Loop------------------
+
+
+// #include<stdio.h>
+// int main(){
+//     for(int i = 1; i<=100;i++){
+//         printf("%d \n", i);
+//     }
+// }
+
+// #include<stdio.h>
+// int main(){
+//     for(int i = 0 ; i <=10 ; i++){
+//         printf("%d \n", i);
+//     }
+// }
+
+
+// #include<stdio.h>
+// int main(){      // Post Increment
+//     int i = 1;
+//     printf("%d \n" , i++);
+//     printf("%d", i);
+// }
+// #include<stdio.h>
+// int main(){      // Pre Increment
+//     int i = 1;
+//     printf("%d \n" , ++i);
+//     printf("%d", i);
+// }
+
+// #include<stdio.h>
+// int main(){
+//     for (float i = 1.00 ; i<=5.00;i++ ){
+//         printf("%f \n" , i);
+//     }
+// }
+// #include<stdio.h>
+// int main(){
+//     for (char ch = 'a' ; ch <='z' ; ch++){
+//         printf("%c \n" , ch);
+//     }
+// }
