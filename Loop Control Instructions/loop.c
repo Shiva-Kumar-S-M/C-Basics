9c79f22c531f862489f8aef3da3e905c41047535
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2024-10-02 11:21:38 +0530

    Do - while

--- Loop Control Instructions/loop.c
+++ Loop Control Instructions/loop.c
@@ -53,18 +53,44 @@
 // }
  
 
-#include<stdio.h>
-int main(){
-    int n;
-    printf("Enter a number:");
-    scanf("%d \n",&n);
-    // int i=1;
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter a number:");
+//     scanf("%d \n",&n);
+//     // int i=1;
 
-    // while(i<=n){
-    //     printf("%d \n",i);
-    //     i++;
-    // }
-    for ( int i = 0; i<=n ; i++){
-        printf("%d \n", i);
-    }
-}
+//     // while(i<=n){
+//     //     printf("%d \n",i);
+//     //     i++;
+//     // }
+//     for ( int i = 0; i<=n ; i++){
+//         printf("%d \n", i);
+//     }
+// }
+
+// // --------Do while ------------
+// #include<stdio.h>
+// int main (){
+//     int i = 1;
+//     do{
+//         printf("%d \n", i);
+//         i++;
+//     } while(i <= 12);
+// }
+
+// // WAP to sum of Natrural Numbers
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter a number:");
+//     scanf("%d " , &n);
+//     int sum =0;
+//     // for(int i=1 , j=n;i<n && j>=1;i++,j--)
+//     for(int j=n; j>=1;j--){
+//         sum = sum +j;
+//         printf("%d \n" , j);
+        
+//     }
+//     printf("Sum of Number is : %d \n ",sum);
+// }
