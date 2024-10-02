a1857323a3ccf8253d3d594b9ef4287f0a1555bf
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2024-10-02 18:34:59 +0530

    Problems

--- Functions & Recuursion/Function.c
+++ Functions & Recuursion/Function.c
@@ -73,3 +73,57 @@
 // int sum(int a,int b){
 //    return a+b;
 // }
+
+//WAP to print table using function
+// #include<stdio.h>
+// void printTable(int n);
+
+// int main(){
+//     int n;
+//     printf("Enter A Numebr:");
+//     scanf("%d",&n);
+//     printTable(n);        //arguments or Actual Parameter
+//     return 0;
+// }
+// void printTable(int n){       //parameter or Formal parameter
+//     for(int i=1;i<=10;i++)
+//     printf("%d \n",n*i);
+// }
+
+
+// #include<stdio.h>
+
+// void calculatePrice(float value);
+
+// int main(){
+//     float value = 100.0;
+//     calculatePrice(value);
+//     return 0;
+
+// }
+
+// void calculatePrice(float value){
+//     value = value +(0.18 * 100.0);
+//     printf("Value after GST is:%f",value);
+
+// }
+
+// using library function calulate sqaure of given number
+// #include<stdio.h>
+// #include<math.h>
+
+// float Sqaure(float n);
+
+// int main(){
+//     int n;
+//     printf("Enter a number:");
+//     scanf("%f",&n);
+//     Square(n);
+//     return 0;
+
+
+// }
+
+// float Square(float n){
+//     printf("The Square of Number is :%f",pow(n,2));
+// }
